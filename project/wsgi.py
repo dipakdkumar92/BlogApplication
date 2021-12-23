f2fd5e05c76368c59b1243dac0eda158a1b2e1be
dipakgupta <dipakdkumar92@gmail.com>
2021-12-23 23:05:37 +0530

    create superuser using wsgi file

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,8 +9,20 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
+from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = get_wsgi_application()
+
+
+users = User.objects.all()
+if not users:
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="super_password",
+        is_active=True,
+        is_staff=True,
+    )
