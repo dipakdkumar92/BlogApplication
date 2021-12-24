077019457d2f74a2749054bc22a4885da50a0ea2
dipakgupta <dipakdkumar92@gmail.com>
2021-12-24 12:50:07 +0530

    Remove superuser

--- project/wsgi.py
+++ project/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
@@ -17,12 +17,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 application = get_wsgi_application()
 
 
-users = User.objects.all()
-if not users:
-    User.objects.create_superuser(
-        username="admin",
-        email="admin@example.com",
-        password="super_password",
-        is_active=True,
-        is_staff=True,
-    )
+# users = User.objects.all()
+# if not users:
+#     User.objects.create_superuser(
+#         username="admin",
+#         email="admin@example.com",
+#         password="super_password",
+#         is_active=True,
+#         is_staff=True,
+#     )
